a9094261b77d638d58f4631e8cb6f3a7edfea0e6
Wafee Qazi <wafqaz8@gmail.com>
2026-07-20 12:55:24 -0400

    Update scenario.js

--- js/scenario.js
+++ js/scenario.js
@@ -1,15 +1,48 @@
-function buildWall() {
+function refillPositionBag() {
+    game.positionBag = [];
+
+    for (let i = 0; i < game.freeKickPositions.length; i++) {
+        game.positionBag.push(i);
+    }
+
+    shufflePositionBag();
+}
+
+function shufflePositionBag() {
+    for (let i = game.positionBag.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+
+        const temp = game.positionBag[i];
+        game.positionBag[i] = game.positionBag[randomIndex];
+        game.positionBag[randomIndex] = temp;
+    }
+}
+
+function getNextFreeKickPosition() {
+    if (game.positionBag.length === 0) {
+        refillPositionBag();
+    }
+
+    const index = game.positionBag.pop();
+
+    game.currentPosition = game.freeKickPositions[index];
+
+    return game.currentPosition;
+}
+
+function createWall() {
     game.wall.length = 0;
 
-    const scenario = game.scenarios[game.currentScenario];
+    const position = game.currentPosition;
 
+    const players = position.wallPlayers;
     const spacing = 40;
 
     const startX =
         game.freeKick.x -
-        ((scenario.wallPlayers - 1) * spacing) / 2;
+        ((players - 1) * spacing) / 2;
 
-    for (let i = 0; i < scenario.wallPlayers; i++) {
+    for (let i = 0; i < players; i++) {
         game.wall.push({
             x: startX + i * spacing,
             y: game.freeKick.y - game.freeKick.wallDistance,
@@ -18,41 +51,25 @@ function buildWall() {
     }
 }
 
-function applyScenario() {
-    const scenario = game.scenarios[game.currentScenario];
+function loadRandomFreeKick() {
+    const position = getNextFreeKickPosition();
 
-    game.freeKick.x = scenario.x;
-    game.freeKick.y = scenario.y;
-    game.freeKick.wallDistance = scenario.wallDistance;
+    game.freeKick.x = position.x;
+    game.freeKick.y = position.y;
+    game.freeKick.wallDistance = position.wallDistance;
 
     game.ball.x = game.freeKick.x;
     game.ball.y = game.freeKick.y;
 
-    buildWall();
+    game.ball.moving = false;
+    game.ball.velocityX = 0;
+    game.ball.velocityY = 0;
+
+    game.ball.curve = 0;
+    game.ball.progress = 0;
+
+    createWall();
 }
 
-function nextScenario() {
-    game.currentScenario++;
-
-    if (game.currentScenario >= game.scenarios.length) {
-        game.currentScenario = 0;
-    }
-
-    applyScenario();
-}
-
-function previousScenario() {
-    game.currentScenario--;
-
-    if (game.currentScenario < 0) {
-        game.currentScenario = game.scenarios.length - 1;
-    }
-
-    applyScenario();
-}
-
-function resetFreeKick() {
-    applyScenario();
-}
-
-resetFreeKick();
+refillPositionBag();
+loadRandomFreeKick();
